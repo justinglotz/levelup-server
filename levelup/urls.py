57e40f5dc6eef1a8edaf6ea7367a655ebc8fad7a
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-05-19 20:23:50 -0500

    event view and get requests working

--- levelup/urls.py
+++ levelup/urls.py
@@ -20,9 +20,11 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from levelupapi.views import GameTypeView
+from levelupapi.views import EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -1,1 +1,2 @@
 from .game_type import GameTypeView
+from .event import EventView
