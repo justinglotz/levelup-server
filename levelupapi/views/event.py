9ce17f6f3542a2972b24024e89be7499990ef453
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-05-20 20:31:34 -0500

    update event

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -50,6 +50,27 @@ class EventView(ViewSet):
         serializer = EventSerializer(event)
         return Response(serializer.data)
 
+    def update(self, request, pk):
+        """Handle PUT requests for an event
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        event = Event.objects.get(pk=pk)
+        event.description = request.data["description"]
+        event.date = request.data["date"]
+        event.time = request.data["time"]
+
+        game = Game.objects.get(pk=request.data["game"])
+        organizer = Gamer.objects.get(pk=request.data["organizer"])
+        event.organizer = organizer
+        event.game = game
+
+        event.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
 
 class EventSerializer(serializers.ModelSerializer):
     """JSON serializer for events
